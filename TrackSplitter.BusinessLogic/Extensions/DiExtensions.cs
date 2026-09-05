fec85d653eceeaab5abc9ae8ea041fc283008ab6
Vladislav Pomozov <vladuk2332@gmail.com>
2026-09-05 19:39:00 +0300

    add rabbitMq in Di

--- TrackSplitter.BusinessLogic/Extensions/DiExtensions.cs
+++ TrackSplitter.BusinessLogic/Extensions/DiExtensions.cs
@@ -1,5 +1,7 @@
+using MassTransit;
 using Microsoft.Extensions.DependencyInjection;
-
+using Microsoft.Extensions.Options;
+using TrackSplitter.BusinessLogic.Options;
 using TrackSplitter.DataAccess.Extensions;
 
 namespace TrackSplitter.BusinessLogic.Extensions;
@@ -11,6 +13,37 @@ public static class DiExtensions
         public IServiceCollection AddBusinessLogicLayer(string connectionString)
         {
             services.AddPostgresDb(connectionString);
+            services.ConfigureOptions();
+            services.AddRabbitMq();
+
+            return services;
+        }
+
+        public IServiceCollection ConfigureOptions()
+        {
+            services
+                .AddOptions<RabbitMqOptions>()
+                .BindConfiguration(RabbitMqOptions.SectionName)
+                .ValidateDataAnnotations()
+                .ValidateOnStart();
+
+            return services;
+        }
+        public IServiceCollection AddRabbitMq()
+        {
+            services.AddMassTransit(registrationConfig =>
+            {
+                registrationConfig.UsingRabbitMq((context, cfg) =>
+                {
+                    var options = context.GetRequiredService<IOptions<RabbitMqOptions>>().Value;
+
+                    cfg.Host(options.ServerHost, hostConfigurator =>
+                    {
+                        hostConfigurator.Username(options.UserName);
+                        hostConfigurator.Password(options.Password);
+                    });
+                });
+            });
 
             return services;
         }
