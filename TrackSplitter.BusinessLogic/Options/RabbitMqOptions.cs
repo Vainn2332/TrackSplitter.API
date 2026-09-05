200b4b6c0989829336739f83262adbbb9a65b941
Vladislav Pomozov <vladuk2332@gmail.com>
2026-09-05 19:38:46 +0300

    add rabbitMqOptions

---
+++ TrackSplitter.BusinessLogic/Options/RabbitMqOptions.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TrackSplitter.BusinessLogic.Options;
+
+public class RabbitMqOptions
+{
+    public const string SectionName = "RabbitMqOptions";
+
+    [Required]
+    public required string ServerHost { get; set; }
+
+    [Required]
+    public required string UserName { get; set; }
+
+    [Required]
+    public required string Password { get; set; }
+}
